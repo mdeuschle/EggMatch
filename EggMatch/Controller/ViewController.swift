d5a8daaf3a59bbcf87b8868cc255b6ceef96e2fe
Matthew Deuschle <mdeuschle@gmail.com>
2019-05-05 19:12:55 -0700

    add outlets

--- EggMatch/Controller/ViewController.swift
+++ EggMatch/Controller/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private var highScoreLabel: UILabel!
+    @IBOutlet private var gamesPlayedLabel: UILabel!
+    @IBOutlet private var flipCountLabel: UILabel!
+    @IBOutlet private var scoreLabel: UILabel!
+    @IBOutlet private var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
